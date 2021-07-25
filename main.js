90adbe8b8d0bdd7cbf605de57440944929374a55
sakakibara-hi <sakakibarahi529@gmail.com>
2021-07-26 08:02:26 +0900

    pre final
    提出前チェック

--- main.js
+++ main.js
@@ -3,4 +3,16 @@ ScrollReveal().reveal('.menu',{
     origin: 'top', 
     distance: '50px',
     viewFactor: 0.5
-  });
+});
+ScrollReveal().reveal('.face',{
+  duration: 1600, 
+  origin: 'left', 
+  distance: '50px',
+  viewFactor: 0.5
+});
+ScrollReveal().reveal('.name',{
+  duration: 1600, 
+  origin: 'left', 
+  distance: '50px',
+  viewFactor: 0.5
+});
